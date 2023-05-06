e11e1837799f18670917780e61b8c7cc3b0e3faa
reDpz <sv_redpz@proton.me>
2023-05-06 23:35:50 +0100

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,21 +9,22 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  'rebelot/kanagawa.nvim',
-  'folke/tokyonight.nvim',
-  'voidekh/kyotonight.vim',
-  'morhetz/gruvbox',
-  
+  -- 'rebelot/kanagawa.nvim',
+  -- 'folke/tokyonight.nvim',
+  -- 'voidekh/kyotonight.vim',
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  -- 'morhetz/gruvbox',
+
   {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true },
     config = function()
-      require('lualine').setup{
+      require('lualine').setup {
         options = {
           icons_enabled = true,
           theme = 'gruvbox',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -38,18 +39,18 @@ return {
           }
         },
         sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
         },
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
           lualine_y = {},
           lualine_z = {}
         },
@@ -206,9 +207,9 @@ let g:mkdp_theme = 'dark']])
   {
     "startup-nvim/startup.nvim",
     lazy = false,
-    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     config = function()
-      require"startup".setup()
+      require "startup".setup()
     end
   },
   {
@@ -216,33 +217,34 @@ let g:mkdp_theme = 'dark']])
     lazy = false,
     config = function()
       require('tabout').setup {
-      tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-      backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-      act_as_tab = true, -- shift content if tab out is not possible
-      act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-      default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-      default_shift_tab = '<C-d>', -- reverse shift default action,
-      enable_backwards = true, -- well ...
-      completion = true, -- if the tabkey is used in a completion pum
-      tabouts = {
-        {open = "'", close = "'"},
-        {open = '"', close = '"'},
-        {open = '`', close = '`'},
-        {open = '(', close = ')'},
-        {open = '[', close = ']'},
-        {open = '{', close = '}'}
-      },
-      ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-      exclude = {} -- tabout will ignore these filetypes
-  }
+        tabkey = '<Tab>',             -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true,            -- shift content if tab out is not possible
+        act_as_shift_tab = false,     -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>',        -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>',  -- reverse shift default action,
+        enable_backwards = true,      -- well ...
+        completion = true,            -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '{', close = '}' }
+        },
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {} -- tabout will ignore these filetypes
+      }
     end,
-    wants = {'nvim-treesitter'}, -- or require if not used so far
-    after = {'nvim-cmp'} -- if a completion plugin is using tabs load it before
+    wants = { 'nvim-treesitter' }, -- or require if not used so far
+    after = { 'nvim-cmp' }         -- if a completion plugin is using tabs load it before
   },
-  {'anuvyklack/pretty-fold.nvim',
-     config = function()
-        require('pretty-fold').setup()
-     end
+  {
+    'anuvyklack/pretty-fold.nvim',
+    config = function()
+      require('pretty-fold').setup()
+    end
   },
   'sainnhe/gruvbox-material',
 }
